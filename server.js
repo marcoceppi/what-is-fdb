b11d32dc297f6cd55ec2a70012b3e1eb0747ade2
Marco Ceppi <marco@ceppi.net>
2014-09-22 21:50:09 -0400

    make it work and don't be stupid

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 var express = require('express')
   , app = express()
   , fdb = require('fdb').apiVersion(200)
-  , dbfile = process.env['FDB_CLUSTER'] || null
-  , db = fdb.open()
+  , dbfile = process.env['FDB_CLUSTER'] || null;
 
-app.get('/', function(req, res) {
-  db.set('yodawg', 'whatup', function(w) {
-    console.log(w);
-    res.send('Hello World');
+fdb.init();
+
+var db = fdb.open()
+
+app.use(function(req, res, next) {
+  db.doTransaction(function(t, icb) {
+    t.get('online', function(err, val) {
+      if(err) return icb(err);
+
+      if(!val) {
+        val = 0;
+      } else {
+        val = parseInt(val.toString());
+      }
+
+      val++;
+
+      t.set('online', val.toString());
+      icb(null, val);
+    });
+  }, function(err, val) {
+    if(!err) {
+      console.log('New vistor, total: ' + val);
+      next();
+    }
   });
 });
 
+app.get('/', function(req, res) {
+  db.set('yodawg', 'whatup');
+  res.send('Hello World');
+});
+
 app.get('/add', function(req, res) {
-  db.get('yodawg', function(v) {
-    console.log(v);
+  db.get('yodawg', function(err, v) {
+    console.log(v.toString());
     res.send(200);
   });
 });
